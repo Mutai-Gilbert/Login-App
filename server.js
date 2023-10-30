a5cfc9a285196b56ad50742eabeb2b8736c18ef6
mutai-gilbert <mutaigilbert85@gmail.com>
2023-10-30 17:55:27 +0300

    Added new changes to  server.js

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-import express from "express"
+import express from "express";
+import path from "path"
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs')
+
+// load static assets
+app.use('/static', express.static(path.join(__dirname, 'public')))
+
+// load the assets image
+app.use('assets', express.static(path.join(__dirname, 'public/assets')))
 
 //HOME ROUTE
 app.get('/', (req, res) => {
