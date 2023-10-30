dec49ecf9a74ff6094446992e718809b62f24f3b
mutai-gilbert <mutaigilbert85@gmail.com>
2023-10-30 20:28:36 +0300

    Add new changes to the server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import path from "path"
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+import session from "express-session";
+import { v4 as uuidv4 } from "uuid";
+import router from './router.js';
 
 
 const __filename = fileURLToPath(import.meta.url)
@@ -10,14 +14,24 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
 // load static assets
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 // load the assets image
-app.use('assets', express.static(path.join(__dirname, 'public/assets')))
+app.use('assets', express.static(path.join(__dirname, 'public/assets')));
 
+app.use(session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true
+}))
+
+
+app.use('/route',router)
 //HOME ROUTE
 app.get('/', (req, res) => {
     res.render('base', { title: "Login System"});
